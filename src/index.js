06fd5a92c83c11c5a69871854b47b9ccd0a00583
Vagyasri <vagyasri2011@gmail.com>
2021-11-10 09:03:10 +0100

    Add Function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css';
+
+const names = ['Hole-in-one!', 'Eagle', 'Birdie', 'Par', 'Bogey', 'Double Bogey', 'Go Home!'];
+
+const golfScore = (par, strokes) => {
+  if (strokes === 1) {
+    return names[0];
+  }
+  if (strokes <= (par - 2)) {
+    return names[1];
+  }
+  if (strokes === (par - 1)) {
+    return names[2];
+  }
+  if (strokes === par) {
+    return names[3];
+  }
+  if (strokes === (par + 1)) {
+    return names[4];
+  }
+  if (strokes === (par + 2)) {
+    return names[5];
+  }
+  if (strokes >= (par + 3)) {
+    return names[6];
+  }
+  return 'Change Me';
+};
+
+golfScore(5, 4);
